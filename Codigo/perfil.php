ef5d9b051a0824263d5d8f46c6fc16a3cf2ebc27
Sol <solbarrionueevo@gmail.com>
2025-08-19 22:02:30 -0300

    feat(perfil): avatar aleatorio si no hay foto de perfil

--- Codigo/perfil.php
+++ Codigo/perfil.php
@@ -17,22 +17,61 @@ $resultado = $stmt->get_result();
 $usuario = $resultado->fetch_assoc();
 
 // Definir avatar por defecto si no hay foto
-$avatar = !empty($usuario['foto_perfil']) ? htmlspecialchars($usuario['foto_perfil']) : "imagenes/avatar_mascota.png";
+if (!empty($usuario['foto_perfil'])) {
+    $avatar = htmlspecialchars($usuario['foto_perfil']);
+} else {
+    // Lista de avatares
+    $avatares = [
+        "imagenes/avatars/avatar1.png",
+        "imagenes/avatars/avatar2.png",
+        "imagenes/avatars/avatar3.png",
+        "imagenes/avatars/avatar4.png",
+        "imagenes/avatars/avatar5.png",
+        "imagenes/avatars/avatar6.png",
+        "imagenes/avatars/avatar7.png",
+        "imagenes/avatars/avatar8.png",
+        "imagenes/avatars/avatar9.png",
+        "imagenes/avatars/avatar10.png",
+        "imagenes/avatars/avatar11.png",
+        "imagenes/avatars/avatar12.png",
+        "imagenes/avatars/avatar13.png",
+        "imagenes/avatars/avatar14.png",
+        "imagenes/avatars/avatar15.png",
+        "imagenes/avatars/avatar16.png",
+        "imagenes/avatars/avatar17.png",
+        "imagenes/avatars/avatar18.png",
+        "imagenes/avatars/avatar19.png",
+        "imagenes/avatars/avatar20.png",
+    ];  
+    // Elegir uno al azar
+    $avatar = $avatares[array_rand($avatares)];
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Mi Perfil - MHAC</title>
+    <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/perfil.css">
+    <a href="index.php" class="btn-volver">
+            <span class="btn-volver-icon">←</span>
+            Volver al inicio
+    </a>
 </head>
 <body>
-
     <div class="perfil-container">
         <h1>Mi Perfil</h1>
+        
+        <div class="avatar-container">
+            <?php if (!empty($usuario['foto_perfil'])): ?>
+                <img src="<?php echo $avatar; ?>" alt="Foto de perfil" class="foto-perfil">
+            <?php else: ?>
+                <img src="<?php echo $avatar; ?>" alt="Avatar por defecto" class="avatar-default">
+            <?php endif; ?>
+        </div>
 
-        <!-- Foto de perfil -->
-        <img src="<?php echo $avatar; ?>" alt="Foto de perfil" class="foto-perfil">
 
         <p><strong>Nombre:</strong> <?php echo htmlspecialchars($usuario['nombre']); ?></p>
         <p><strong>Email:</strong> <?php echo htmlspecialchars($usuario['email']); ?></p>
